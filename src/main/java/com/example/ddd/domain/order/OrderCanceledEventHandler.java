9e4fa0101ddfed476f51cc62cd4adf646e9704ce
k-gn <rlarbska97@gmail.com>
2023-05-03 13:52:42 +0900

    feat: add async

--- src/main/java/com/example/ddd/domain/order/OrderCanceledEventHandler.java
+++ src/main/java/com/example/ddd/domain/order/OrderCanceledEventHandler.java
@@ -3,6 +3,7 @@ package com.example.ddd.domain.order;
 import com.example.ddd.application.order.RefundService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 // 이벤트를 처리할 핸들러
@@ -12,6 +13,8 @@ public class OrderCanceledEventHandler {
 
     private final RefundService refundService;
 
+    // 별도의 스레드로 비동기 실행 (외부 서비스 이슈 해결)
+    @Async
     @EventListener(OrderCanceledEvent.class)
     public void handle(OrderCanceledEvent event) {
         refundService.refund(event.getOrderNumber());
